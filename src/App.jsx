b900d58f83ea89cd3ea5b6618155b2d09a7b63b6
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2019-11-19 19:00:42 +0100

    Added ThemeProvider, Main and Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 ﻿import React from 'react';
 
+// MUI
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+import palette from './palette';
+
+// Custom components
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={palette}>
+        <Header />
+        <Main />
+      </ThemeProvider>
+    </>
+  );
 }
