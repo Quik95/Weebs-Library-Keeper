fef3536115224d9bd926b9e074cc0a1774cada85
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2019-11-20 17:45:24 +0100

    Fix makeStyles import

--- src/components/Main/Main.styles.js
+++ src/components/Main/Main.styles.js
@@ -1,4 +1,4 @@
-import makeStyles from '@material-ui/styles/makeStyles';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
   main: { marginTop: theme.spacing(2) },
