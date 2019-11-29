0bd0f4659c8db66b40ac5841c5ab0f59599436b3
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2019-11-29 18:40:25 +0100

    Change file structure

---
+++ src/components/WatchingProgress/WatchingProgress.jsx
@@ -0,0 +1,37 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+
+// Custom components
+import ProgressBar from './ProgressBar/ProgressBar';
+
+// styles
+import useStyles from './WatchingProgress.styles';
+
+const WatchingProgress = memo(function WatchingProgress(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.watchingProgress} data-testid="WatchingProgress">
+      <Typography variant="caption">10/15</Typography>
+      <div className={classes.progressBarContainer}>
+        <ProgressBar
+          className="progress--watched"
+          tooltipText="Number of episodes you watched"
+        />
+        <ProgressBar
+          className="progress--aired"
+          tooltipText="Number of episodes aired"
+        />
+        <ProgressBar
+          className="progress--total"
+          tooltipText="Total number of episodes"
+        />
+      </div>
+    </div>
+  );
+});
+
+WatchingProgress.propTypes = {};
+
+export default WatchingProgress;
