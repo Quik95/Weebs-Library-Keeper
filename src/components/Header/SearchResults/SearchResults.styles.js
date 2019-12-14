b9acc7b712187c77746d9d28854762f1201c03d7
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2019-12-14 21:19:06 +0100

    Adjusted SearchResults height for smaller viewports

--- src/components/Header/SearchResults/SearchResults.styles.js
+++ src/components/Header/SearchResults/SearchResults.styles.js
@@ -12,6 +12,9 @@ export default makeStyles(theme => ({
       '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
     borderRadius: 3,
     marginTop: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      maxHeight: '85vh'
+    }
   },
   listItemText: {
     color: theme.palette.getContrastText(theme.palette.background.paper),
