d6120de9dd2eda90d53b6f03de0a18f9cc5a2400
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2019-12-15 17:58:53 +0100

    Removed unnecessary import

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 // MUI
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import { Typography } from '@material-ui/core';
 
 // styles
 import useStyles from './Main.styles.js';
