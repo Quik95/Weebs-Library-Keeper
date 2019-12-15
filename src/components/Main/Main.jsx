507d02a9a16b52873ce05a95d8c0656e0b3a1868
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2019-12-15 18:48:22 +0100

    Changes for extra large viewports

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,12 +10,16 @@ import useStyles from "./Main.styles.js";
 // Custom components
 import AnimeCard from "../AnimeCard/AnimeCard";
 
+//hooks
+import useBreakpoint from "../../hooks/useBreakpoint";
+
 export default function Main() {
   const classes = useStyles();
+  const matchesXl = useBreakpoint("xl");
 
   return (
     <main className={classes.main} data-testid="main">
-      <Container maxWidth="lg">
+      <Container maxWidth={matchesXl ? "xl" : "lg"}>
         <Grid container spacing={6}>
           {[0, 1, 2, 3, 4, 5].map(i => (
             <AnimeCard key={i} />
