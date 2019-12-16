dbf34854814ce26e3a82721e4fe30456b6a4ae6f
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2019-12-16 17:11:41 +0100

    Configured CORS

--- server/app.js
+++ server/app.js
@@ -3,16 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const apiRouter = require('./routes/api');
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+}
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(cors(corsOptions))
 
 app.use('/api', apiRouter);
 app.get('*', (req, res) => {
